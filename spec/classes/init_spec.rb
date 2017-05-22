e5746bfddc73ccdc337ecd9e1a350166e871b00c
Oliver Bertuch <o.bertuch@fz-juelich.de>
2017-05-22 22:25:52 +0200

    Extending rspec test of certmgmt class to ensure proper handling of certs given via hiera

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,14 +7,46 @@ describe 'certmgmt' do
         facts
       end
 
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_class('certmgmt') }
+      context "without parameters" do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('certmgmt') }
+      end
 
-      case facts[:osfamily]
-      when 'RedHat'
+      context "with certs in parameters" do
+        # load certificats to be used later on
+        certs = { 'long' => {}, 'short' => {}}
+        # cert with a long chain (more than 1: root + intermediate)
+        certs['long']['chain'] = {
+          'inter' => IO.read('spec/fixtures/files/chained_ca/inter/inter.pem'),
+          'root' => IO.read('spec/fixtures/files/chained_ca/root/root.pem')
+        }
+        certs['long']['x509'] = IO.read('spec/fixtures/files/chained_ca/server.pem')
+        certs['long']['key'] = IO.read('spec/fixtures/files/chained_ca/server.key')
+
+        # cert with a short chain (all certs directly signed by root)
+        certs['short']['chain'] = IO.read('spec/fixtures/files/direct_ca/root.pem')
+        certs['short']['x509'] = IO.read('spec/fixtures/files/direct_ca/server.pem')
+        certs['short']['key'] = IO.read('spec/fixtures/files/direct_ca/server.key')
+
+        certs.each do |type, data|
+          context "with #{type} chain of ca certs" do
+            let(:params) do
+              {
+                :certs => {'test' => data}
+              }
+            end
+
+            it { is_expected.to compile.with_all_deps }
+            it { is_expected.to contain_class('certmgmt') }
+            it { is_expected.to contain_certmgmt__cert('test')}
+          end
+        end
+      end
+      #case facts[:osfamily]
+      #when 'RedHat'
         #it { is_expected.to contain_package('xyz') }
         #...
-      end
+      #end
     end
   end
 end
