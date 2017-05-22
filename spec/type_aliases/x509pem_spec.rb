5640c5defbcb5b20bdd74bcd68c436e632a0764e
Oliver Bertuch <o.bertuch@fz-juelich.de>
2017-05-22 16:04:40 +0200

    Add rspec test for Certmgmt::KeyPem type

--- spec/type_aliases/x509pem_spec.rb
+++ spec/type_aliases/x509pem_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Certmgmt::X509pem', :type => :type_alias, :if => Puppet::Util::Package.versioncmp(Puppet.version, '4.4.0') >= 0 do
+describe 'Certmgmt::X509pem', :if => Puppet::Util::Package.versioncmp(Puppet.version, '4.4.0') >= 0 do
   cert = IO.read('spec/fixtures/files/testcert.pem')
   it { is_expected.to allow_value(cert) }
 end
