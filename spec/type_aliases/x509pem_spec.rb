502833f13b5ad679effc18e624c034651a10b3ab
Oliver Bertuch <o.bertuch@fz-juelich.de>
2017-05-22 15:19:47 +0200

    Use test cert for type alias rspec test

--- spec/type_aliases/x509pem_spec.rb
+++ spec/type_aliases/x509pem_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
-describe 'Certmgmt::X509PEM', :type => :type_alias do
-  it { is_expected.to allow_value('square') }
+describe 'Certmgmt::X509pem', :type => :type_alias, :if => Puppet::Util::Package.versioncmp(Puppet.version, '4.4.0') >= 0 do
+  cert = IO.read('spec/fixtures/files/testcert.pem')
+  it { is_expected.to allow_value(cert) }
 end
