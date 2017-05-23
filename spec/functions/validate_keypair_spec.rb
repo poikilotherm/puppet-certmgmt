45a84a1739f54edc6b9f2b3313cb403368e16b5c
Oliver Bertuch <o.bertuch@fz-juelich.de>
2017-05-23 14:20:11 +0200

    Make rubocop happy with validate_keypair_spec rspec test

--- spec/functions/validate_keypair_spec.rb
+++ spec/functions/validate_keypair_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
-describe 'certmgmt::validate_keypair', :type => :puppet_function do
+describe 'certmgmt::validate_keypair' do
   title = 'test'
   cert_match = IO.read('spec/fixtures/files/direct_ca/server.pem')
   key_match = IO.read('spec/fixtures/files/direct_ca/server.key')
   key_fail = IO.read('spec/fixtures/files/direct_ca/root.key')
 
-  it "should run with matching public/private keypair" do
-    is_expected.to run.with_params(title, cert_match, key_match)
+  it 'runs with certmgmt::validate_keypair("<title>", "<valid cert here>", "<valid key here>") and return true' do
+    is_expected.to run.with_params(title, cert_match, key_match).and_return(true)
   end
-  it "should fail with non-matching public/private keypair" do
-    is_expected.not_to run.with_params(title, cert_match, key_fail)
+  it 'fails with certmgmt::validate_keypair("<title>", "<valid cert here>", "<valid key here>") but not matching pair and raise exception' do
+    is_expected.to run.with_params(title, cert_match, key_fail).and_raise_error(Puppet::ParseError)
   end
-  it "should fail with non-valid X509 certificate" do
-    is_expected.not_to run.with_params(title, "cert_match", key_fail)
+  it 'fails with certmgmt::validate_keypair("<title>", "invalid cert", "<valid key here>") and raise exception' do
+    is_expected.to run.with_params(title, 'cert_match', key_fail).and_raise_error(Puppet::ParseError)
   end
-  it "should fail with non-valid private key" do
-    is_expected.not_to run.with_params(title, cert_match, "key_fail")
+  it 'fails with certmgmt::validate_keypair("<title>", "<valid cert here>", "invalid key") and raise exception' do
+    is_expected.to run.with_params(title, cert_match, 'key_fail').and_raise_error(Puppet::ParseError)
   end
 end
