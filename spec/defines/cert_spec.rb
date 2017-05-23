a391c9b14a477566fd55a6fe8ed748c89b871f24
Oliver Bertuch <o.bertuch@fz-juelich.de>
2017-05-23 14:12:05 +0200

    Make rubocop happy with cert_spec rspec test

--- spec/defines/cert_spec.rb
+++ spec/defines/cert_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe 'certmgmt::cert', :type => :define do
+describe 'certmgmt::cert' do
   let(:pre_condition) { 'class {"::certmgmt": }' }
   let(:title) do
     '#test'
   end
 
   # load certificats to be used later on
-  certs = { 'long' => {}, 'short' => {}}
+  certs = { 'long' => {}, 'short' => {} }
   join_chain = {}
   # cert with a long chain (more than 1: root + intermediate)
   certs['long']['chain'] = {
@@ -16,7 +16,7 @@ describe 'certmgmt::cert', :type => :define do
   }
   certs['long']['x509'] = IO.read('spec/fixtures/files/chained_ca/server.pem')
   certs['long']['key'] = IO.read('spec/fixtures/files/chained_ca/server.key')
-  join_chain['long'] = certs['long']['chain']['inter']+certs['long']['chain']['root']
+  join_chain['long'] = certs['long']['chain']['inter'] + certs['long']['chain']['root']
 
   # cert with a short chain (all certs directly signed by root)
   certs['short']['chain'] = IO.read('spec/fixtures/files/direct_ca/root.pem')
@@ -36,12 +36,12 @@ describe 'certmgmt::cert', :type => :define do
         context "with #{type} chain of ca certs" do
           combos.each do |combo|
             context "and combining = #{combo}" do
-              content = ""
+              content = ''
               case combo
               when true
                 content = "#{data['key']}#{data['x509']}#{join_chain[type]}"
               when false
-                content = "#{data['x509']}"
+                content = data['x509'].to_s
               when 'key+cert'
                 content = "#{data['key']}#{data['x509']}"
               when 'cert+chain'
@@ -49,10 +49,10 @@ describe 'certmgmt::cert', :type => :define do
               end
 
               let(:params) do
-                data.merge({
-                  :combined => combo,
-                  :file => '/etc/pki/tls/server.pem'
-                })
+                data.merge(
+                  combined: combo,
+                  file: '/etc/pki/tls/server.pem'
+                )
               end
 
               it "should compile and contain resource for #{type} chains" do
@@ -66,15 +66,7 @@ describe 'certmgmt::cert', :type => :define do
           end
         end
       end
-
-      case facts[:osfamily]
-      when 'RedHat'
-        #it { is_expected.to contain_package('xyz') }
-        #...
-      end
     end
   end
 end
-
-
 # vim: sw=2 ts=2 sts=2 et :
